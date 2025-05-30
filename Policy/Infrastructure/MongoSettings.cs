2ccc5e13f85cb75b2e336d9d70e133d5f9f0bb46
joya-sakr <99543239+joya-sakr@users.noreply.github.com>
2025-05-30 10:37:17 +0300

    Add files via upload

---
+++ Policy/Infrastructure/MongoSettings.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Policy.Domain
+{
+    public class MongoSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string PoliciesCollectionName { get; set; } = null!;
+        public string ClaimsCollectionName { get; set; } = null!;
+    }
+}
+

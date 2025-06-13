4a3e257c84cc4453be1e6b0b5bc8fca42d57dab3
joya-sakr <99543239+joya-sakr@users.noreply.github.com>
2025-06-13 09:05:16 +0300

    anthC and authZ

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -3,10 +3,24 @@ using Ocelot.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "http://localhost:5271"; 
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
 builder.Services.AddOcelot(builder.Configuration);
 
 
@@ -16,12 +30,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
 
- app.UseSwagger();
- app.UseSwaggerUI();
-
-
-app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
